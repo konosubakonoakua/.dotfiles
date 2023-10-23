009eca4d7eabae7afc37d6663cc02172a85633b4
konosubakonoakua <ailike_meow@qq.com>
2023-10-23 13:27:08 +0800

    feat(scripts): add neovide install script

---
+++ scripts/install/install_neovide.sh
@@ -0,0 +1,14 @@
+./install_from_github_release.sh \
+	~/Downloads/neovide \
+	~/.local/bin \
+	'https://github.com' \
+	'neovide/neovide/releases' \
+	'neovide/neovide/releases/download' \
+	'>[[:digit:]]{1,2}\.[[:digit:]]{1,2}\.[[:digit:]]{1,2}' \
+	's/>//' \
+	'' \
+	'' \
+	'neovide' \
+	'neovide-linux-x86_64.tar.gz' \
+	'zip' \
+	'tar -zxvf'
