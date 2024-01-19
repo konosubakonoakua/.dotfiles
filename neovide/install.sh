dd43e25d1f1bb2c978dbfababb108f1be6cb13fa
konosubakonoakua <ailike_meow@qq.com>
2024-01-19 17:51:34 +0800

    fix(neovide): skip `check()` for `neovide`

--- neovide/install.sh
+++ neovide/install.sh
@@ -2,4 +2,16 @@
 
 source "$(cd -P "$(dirname "$0")" && pwd -P)/../base.sh"
 
+dst_conf_dir="$HOME/.cofig/neovide"
+mkdir -p "dst_conf_dir"
+
+check() {
+    return 0 # do not check for neovide
+}
+
+do_install() {
+    link_file "$src_conf/config.toml" "$dst_conf_dir/config.toml"
+    return "$?"
+}
+
 install
