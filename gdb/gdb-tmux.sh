82a61028581223909a8ce68c691a9522764c2a7c
konosubakonoakua <ailike_meow@qq.com>
2024-01-29 16:28:55 +0800

    feat(gdb): add `gdb-tmux` for gdb-dashboard
    will append `gdb-tmux` to `.bashrc`

---
+++ gdb/gdb-tmux.sh
@@ -0,0 +1,10 @@
+################################################################
+##### gdb-dashboard tmux
+gdb-tmux() {
+    local id="$(tmux split-pane -hPF "#D" "tail -f /dev/null")"
+    tmux last-pane
+    local tty="$(tmux display-message -p -t "$id" '#{pane_tty}')"
+    gdb -ex "dashboard -output $tty" "$@"
+    tmux kill-pane -t "$id"
+}
+################################################################
--- gdb/install.sh
+++ gdb/install.sh
@@ -5,7 +5,10 @@ source "$(cd -P "$(dirname "$0")" && pwd -P)/../base.sh"
 do_install() {
     # curl -L https://raw.githubusercontent.com/hugsy/gef/main/gef.py -o "$HOME/.gitinit"
     # wget -P ~ https://git.io/.gdbinit || \
-        link_file "$src_conf/gdbinit" "$HOME/.gdbinit"
+    link_file "$src_conf/gdbinit" "$HOME/.gdbinit"
+    if [[ ! "$(cat $HOME/.bashrc)" =~ "gdb-tmux" ]]; then
+        cat ./gdb-tmux.sh >> "$HOME/.bashrc"
+    fi
 }
 
 install -c 'gdb'
