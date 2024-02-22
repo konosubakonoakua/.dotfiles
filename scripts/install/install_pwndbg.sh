e14bf01c6909b589964f5d486d74055a0e5bef69
konosubakonoakua <ailike_meow@qq.com>
2024-02-22 10:45:17 +0800

    feat(script): add installer for pwndbg

---
+++ scripts/install/install_pwndbg.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+source gh_api.sh
+url=$(get_github_release_urls pwndbg/pwndbg | grep amd64-portable)
+
+cd ~/Downloads/ && mkdir -p pwndbg
+cd pwndbg
+wget $url && tar -vxf *.tar.gz
+rm -rf ~/.local/opt/pwndbg
+cp -rf pwndbg ~/.local/opt/pwndbg
+
+if [[ "$(cat $HOME/.bashrc)" =~ "/opt/pwndbg/bin" ]]; then
+    echo '.bashrc already configured!'
+else
+    echo 'export PATH=$PATH:$HOME/.local/opt/pwndbg/bin' >> "$HOME/.bashrc"
+    echo '.bashrc updated!'
+fi
