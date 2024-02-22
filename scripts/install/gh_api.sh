4eb4cc1918622e3bc230e7e29b236047ac07069b
konosubakonoakua <ailike_meow@qq.com>
2024-02-22 10:14:50 +0800

    feat(script): add shell function to get github release url

--- scripts/install/gh_api.sh
+++ scripts/install/gh_api.sh
@@ -1,5 +1,5 @@
 # https://github.com/konosubakonoakua/installer
-LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
-curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
-tar xf lazygit.tar.gz lazygit
-sudo install lazygit /usr/local/bin
+get_github_release_urls() {
+  [ -z $1 ] && return 1
+  curl -s "https://api.github.com/repos/$1/releases/latest" | grep -Po '"browser_download_url": "\K[^"]*'
+}
