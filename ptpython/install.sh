3dca20ce5d2f3e383b7a6b289d81db2d8f697680
konosubakonoakua <ailike_meow@qq.com>
2024-02-29 16:32:13 +0800

    feat(script): add ptpython config

---
+++ ptpython/install.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+source "$(cd -P "$(dirname "$0")" && pwd -P)/../base.sh"
+
+dst_conf_dir="$HOME/.config/ptpython"
+mkdir -p "$dst_conf_dir"
+
+check() {
+    return 0 # do not check for neovide
+}
+
+do_install() {
+    link_file "$src_conf/config.py" "$dst_conf_dir/config.py"
+    return "$?"
+}
+
+install
