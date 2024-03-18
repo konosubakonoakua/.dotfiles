5d7239d4c4113ac16a7ea1a9bd066da91a1fccb1
konosubakonoakua <ailike_meow@qq.com>
2024-03-18 15:32:03 +0800

    feat(script): add installer for `miller`

---
+++ scripts/install/install_miller.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+source gh_api.sh
+url=$(get_github_release_urls johnkerl/miller | grep linux-amd64.tar.gz)
+
+cd ~/Downloads/ && mkdir -p miller
+cd miller
+wget $url && tar -vxf *.tar.gz
+cp -f **/mlr ~/.local/bin/mlr
+
+if [[ "$(cat $HOME/.bashrc)" =~ ".local/bin" ]]; then
+    echo '.bashrc already configured!'
+else
+    echo 'export PATH=$PATH:$HOME/.local/bin' >> "$HOME/.bashrc"
+    echo '.bashrc updated!'
+fi
