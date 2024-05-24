5cc01d508e3bcd6c9583539fb51699e09de77868
konosubakonoakua <ailike_meow@qq.com>
2024-05-25 02:11:48 +0800

    feat(scripts/install): add tree-sitter cli installer

---
+++ scripts/install/install_tree-sitter.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+source gh_api.sh
+url=$(get_github_release_urls tree-sitter/tree-sitter | grep tree-sitter-linux-x64.gz)
+echo $url
+
+cd ~/Downloads/ && mkdir -p tree-sitter
+cd tree-sitter
+rm -f ./* ~/.local/bin/tree-sitter
+wget $url && gzip -d *.gz
+chmod +x ./tree-sitter-linux-x64
+cp -f ./tree-sitter-linux-x64 ~/.local/bin/tree-sitter
+
+if [[ "$(cat $HOME/.bashrc)" =~ ".local/bin" ]]; then
+    echo '.bashrc already configured!'
+else
+    echo 'export PATH=$PATH:$HOME/.local/bin' >> "$HOME/.bashrc"
+    echo '.bashrc updated!'
+fi
