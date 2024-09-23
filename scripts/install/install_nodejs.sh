6e33f9177f6032b0b7494f1e09ce4231eb658e3c
konosubakonoakua <ailike_meow@qq.com>
2024-09-23 16:41:26 +0800

    feat(scripts/nodejs): add installer

--- scripts/install/install_nodejs.sh
+++ scripts/install/install_nodejs.sh
@@ -1,46 +1,20 @@
 #!/bin/bash
 
-#INFO: tested, works good
+# installs fnm (Fast Node Manager)
+curl -fsSL https://fnm.vercel.app/install | bash
 
-PROXY_URL=http://127.0.0.1:3128
-PKG_FOLDER=~/Downloads/nodejs
-INSTALL_PREFIX=$HOME/.local/lib/nodejs
+sleep 1
 
-version_latest=$(
-	curl -L https://nodejs.org/dist/latest/ |
-		grep -oE 'v[[:digit:]]{1,2}\.[[:digit:]]{1,2}\.[[:digit:]]{1,2}' |
-		sort -r --version-sort | uniq | sed 's/v//' | awk 'NR==1{print $1}'
-)
-echo "Found nodejs latest veriosn: $version_latest"
-nodejs_pkg_file=node-v$version_latest-linux-x64.tar.xz
-nodejs_pkg_folder=node-v$version_latest-linux-x64
+# activate fnm
+source ~/.bashrc
 
-mkdir -p $PKG_FOLDER && cd $PKG_FOLDER
+# download and install Node.js
+fnm use --install-if-missing 20
 
-if [[ ! -f $nodejs_pkg_file ]]; then
-	wget https://nodejs.org/dist/latest/$nodejs_pkg_file
-	[[ ! $? -eq 0 ]] && echo "$nodejs_pkg_file download failed..." && exit 1
-else
-	echo "$nodejs_pkg_file already exits."
-	echo "Use cached version."
-fi
+# verifies the right Node.js version is in the environment
+node -v # should print `v20.17.0`
 
-if [[ ! -e $INSTALL_PREFIX ]]; then
-	mkdir $INSTALL_PREFIX && tar -Jxvf $nodejs_pkg_file -C $INSTALL_PREFIX
-	echo "export PATH=$INSTALL_PREFIX/$nodejs_pkg_folder/bin:\$PATH" >>~/.bashrc
-	source ~/.bashrc
-else
-	echo "export PATH=$INSTALL_PREFIX/$nodejs_pkg_folder/bin:\$PATH" >>~/.bashrc
-	echo "nodejs already installed"
-fi
+# verifies the right npm version is in the environment
+npm -v # should print `10.8.2`
 
-if [[ $? -eq 0 ]]; then
-	echo "nodejs installed to $INSTALL_PREFIX/$nodejs_pkg_folder"
-	echo "SET NPM PROXY: $PROXY_URL"
-	npm config set registry http://registry.npmjs.org/
-	npm config set proxy $PROXY_URL
-	npm config set https-proxy $PROXY_URL
-	npm config set strict-ssl false
-else
-	echo "nodejs install failed"
-fi
+npm config set registry https://registry.npmmirror.com
